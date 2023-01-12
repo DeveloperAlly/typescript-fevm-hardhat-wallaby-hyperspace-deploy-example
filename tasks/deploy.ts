b2b91a35ca0f2eb7f2597e26f6a6561cb2031a86
Alison Haire <developerally@Alisons-MBP-2.gateway>
2023-01-12 16:08:15 +1100

    add utils tasks in separate file and update rpc call task

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,10 +1,5 @@
-import { subtask, task, types } from 'hardhat/config';
+import { task, types } from 'hardhat/config';
 import type { TaskArguments } from 'hardhat/types';
-import fs from 'fs';
-import path from 'path';
-import util from 'util';
-const request = util.promisify(require('request'));
-const appendFile = util.promisify(fs.appendFile);
 
 import type { Greeter } from '../typechain-types/Greeter';
 import type { Greeter__factory } from '../typechain-types/factories/Greeter__factory';
@@ -44,44 +39,4 @@ task('deploy:Greeter-Wallaby')
         explorerUrl: `https://explorer.glif.io/address/${greeter.address}/?network=wallabynet`,
       },
     });
-  });
-
-// TODO: typesafety and error checks - im not checking I have method or params
-subtask('callRPC', 'callsWallabyRPC')
-  .addParam('method', 'http method', 'POST', types.string)
-  .addOptionalVariadicPositionalParam('params', 'http body params', [])
-  .setAction(async (taskArguments: TaskArguments) => {
-    console.log('callRPC', taskArguments);
-    var options = {
-      method: 'POST',
-      url: 'https://wallaby.node.glif.io/rpc/v0', //addParam('rpc')
-      headers: {
-        'Content-Type': 'application/json', //addParam('content-type)
-      },
-      body: JSON.stringify({
-        jsonrpc: '2.0',
-        method: taskArguments.method,
-        params: taskArguments.params,
-        id: 1,
-      }),
-    };
-    const res = await request(options);
-    console.log('callRPC res', res.statusMessage);
-    return JSON.parse(res.body).result;
-  });
-
-// TODO: type safety and error checks
-// won't worry about optimising with createWriteStream here since it's only a small about of data
-subtask('logToFile', 'writes outputs to a file')
-  .addParam('filePath', 'relative file path', './log.txt', types.string)
-  .addParam('data', '{json obj data}', {}, types.json)
-  .setAction(async (taskArguments: TaskArguments) => {
-    console.log('writing to file', taskArguments.data);
-    //addFileSync stops anything else that is executing while this runs
-    //won't create the file if it does not exist though so using addFile
-    await appendFile(
-      path.resolve(__dirname, taskArguments.filePath),
-      `${JSON.stringify(taskArguments.data)}\n`,
-      'utf-8'
-    );
   });
---
+++ tasks/utils.ts
@@ -0,0 +1,54 @@
+import { task, types } from 'hardhat/config';
+import type { TaskArguments } from 'hardhat/types';
+import fs from 'fs';
+import path from 'path';
+import util from 'util';
+const request = util.promisify(require('request'));
+const appendFile = util.promisify(fs.appendFile);
+const defaultRPC = 'https://wallaby.node.glif.io/rpc/v0';
+
+// TODO: update params, errorchecking
+task('callRPC', 'callsWallabyRPC')
+  .addParam('method', 'http method', 'POST', types.string)
+  .addOptionalParam(
+    'rpcUrl',
+    'rpc url (wallaby default)',
+    defaultRPC,
+    types.string
+  )
+  .addOptionalVariadicPositionalParam('params', 'http body params', [])
+  .setAction(async ({ rpcUrl, method, params }: TaskArguments) => {
+    console.log('callRPC', { rpcUrl, method, params });
+    var options = {
+      method: method,
+      url: rpcUrl,
+      headers: {
+        'Content-Type': 'application/json', //addParam('content-type)
+      },
+      body: JSON.stringify({
+        jsonrpc: '2.0',
+        method: method,
+        params: params,
+        id: 1,
+      }),
+    };
+    const res = await request(options);
+    console.log('callRPC res', res.statusMessage);
+    return JSON.parse(res.body).result;
+  });
+
+// TODO: error checks
+// won't worry about optimising with createWriteStream here since it's only a small about of data
+task('logToFile', 'writes outputs to a file')
+  .addParam('filePath', 'relative file path', './log.txt', types.string)
+  .addParam('data', '{json obj data}', {}, types.json)
+  .setAction(async (taskArguments: TaskArguments) => {
+    console.log('writing to file', taskArguments.data);
+    //addFileSync stops anything else that is executing while this runs
+    //won't create the file if it does not exist though so using addFile
+    await appendFile(
+      path.resolve(__dirname, taskArguments.filePath),
+      `${JSON.stringify(taskArguments.data)}\n`,
+      'utf-8'
+    );
+  });
